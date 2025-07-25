8ae24c1e141430223b6178f35950d1264ea004b4
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-07-25 16:26:46 -0400

    Add Interval <-> Interval conversion

--- pkg/interval.go
+++ pkg/interval.go
@@ -35,6 +35,22 @@ func (i Interval) Cents() float64 {
 	return float64(i.steps) * 1200.0 / float64(i.edo)
 }
 
+func (i Interval) Approximate12EDOInterval() ApproximateEDOInterval {
+	return i.ApproximateEDOInterval(12)
+}
+
+func (i Interval) ApproximateEDOInterval(edo uint) ApproximateEDOInterval {
+	sourceCents := i.Cents()
+
+	targetStepCents := 1200.0 / float64(edo)
+	targetCents := math.Round(sourceCents/targetStepCents) * targetStepCents
+
+	return ApproximateEDOInterval{
+		Interval{uint(targetCents / targetStepCents), edo},
+		sourceCents - targetCents,
+	}
+}
+
 func (i Interval) dyad() internal.Dyad {
 	stepRatio := math.Pow(2, 1.0/float64(i.edo))
 	intervalRatio := math.Pow(stepRatio, float64(i.steps))
--- pkg/interval_test.go
+++ pkg/interval_test.go
@@ -1,6 +1,9 @@
 package intonation
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestIntervalCents(t *testing.T) {
 	t.Run("12 EDO unison", func(t *testing.T) {
@@ -26,4 +29,66 @@ func TestIntervalCents(t *testing.T) {
 			t.Errorf("Expected cents of 442.1053, got %.4f", i.Cents())
 		}
 	})
+}
+
+func TestApproximateEDOIntervalFromInterval(t *testing.T) {
+	type testCase struct {
+		description      string
+		interval         Interval
+		edo              uint
+		expectedInterval Interval
+		expectedCents    float64
+	}
+
+	testCases := []testCase{
+		{"unison 12 <-> 19", NewInterval(0, 12), 19, Interval{0, 19}, 0.0},
+		{"unison 15 <-> 19", NewInterval(0, 15), 19, Interval{0, 19}, 0.0},
+		{"major 3rd 12 <-> 17", NewInterval(4, 12), 17, Interval{6, 17}, -23.5294},
+		{"major 6th 12 <-> 12", NewInterval(9, 12), 12, Interval{9, 12}, 0.0},
+		{"6/13 <-> 23", NewInterval(6, 13), 23, Interval{11, 23}, -20.0669},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.description, func(t *testing.T) {
+			a := tt.interval.ApproximateEDOInterval(tt.edo)
+
+			if a.Interval() != tt.expectedInterval {
+				t.Errorf("Expected %q, got %q", tt.expectedInterval, a.Interval())
+			}
+
+			if math.Abs(a.CentsOffset()-tt.expectedCents) > EPSILON {
+				t.Errorf("Expected %.4f cents offset, got %.4f", tt.expectedCents, a.CentsOffset())
+			}
+		})
+	}
+}
+
+func TestApproximate12EDOIntervalFromInterval(t *testing.T) {
+	type testCase struct {
+		description   string
+		interval      Interval
+		expectedSteps uint
+		expectedCents float64
+	}
+
+	testCases := []testCase{
+		{"6/13 -> 12", NewInterval(6, 13), 6, -46.1538},
+		{"6/19 -> 12", NewInterval(6, 19), 4, -21.0526},
+		{"6/18 -> 12", NewInterval(6, 18), 4, 0.0},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.description, func(t *testing.T) {
+			expected := NewInterval(tt.expectedSteps, 12)
+			a := tt.interval.Approximate12EDOInterval()
+
+			if a.Interval() != expected {
+				t.Errorf("Expected %q, got %q", expected, a.Interval())
+			}
+
+			if math.Abs(a.CentsOffset()-tt.expectedCents) > EPSILON {
+				t.Errorf("Expected %.4f cents offset, got %.4f", tt.expectedCents, a.CentsOffset())
+			}
+		})
+	}
 }
