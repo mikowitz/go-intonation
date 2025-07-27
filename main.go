1663ff993f8413113eac12cf908b69f58df6c174
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-07-27 17:11:48 -0400

    First bit of CLI creation
    * handles `ratio` and `-h`/`--help` subcommands

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import intonation "github.com/mikowitz/intonation/pkg"
+import (
+	"github.com/mikowitz/intonation/cmd"
+)
 
 func main() {
-	r := intonation.NewRatio(5, 4)
-	r.Play()
-
-	i := intonation.NewInterval(4, 12)
-	i.Play()
+	cmd.Run()
 }
